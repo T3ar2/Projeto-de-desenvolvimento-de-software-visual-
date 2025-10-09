afece65846749c3e425552d84e6edd97a4a9d75f
T3ar2 <nicollashubieciola@gmail.com>
2025-10-09 16:05:38 -0300

    Adição dos cruds nos Endpoints.

---
+++ Academia/API/Endpoints/AlunoEndpoints.cs
@@ -0,0 +1,10 @@
+using API.Data;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Endpoints;
+
+public class AlunoEndpoints
+{
+    
+}
--- Academia/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ Academia/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f2fca464c03acc06fb4c0d06c8924bc047dca67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+450e38ecddc15750fc22eda39276c4454790f4f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
